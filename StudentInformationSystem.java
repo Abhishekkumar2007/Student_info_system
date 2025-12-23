3b9ca137f58b4a5f3a4254211f5ce2ab937b802d
Abhishekkumar2007 <kumarabhishek81457@gmail.com>
2025-12-23 21:11:38 +0530

    Add files via upload

---
+++ StudentInformationSystem.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class StudentInformationSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StudentManager manager = new StudentManager();
+
+        System.out.println("Student Information System Started");
+    }
+}
